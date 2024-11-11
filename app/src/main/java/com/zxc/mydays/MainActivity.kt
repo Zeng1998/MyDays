f2b85576d86e4e69d0d0d46f894f905229acddee
zxc <acan.coder@gmail.com>
2024-11-11 22:19:46 +0800

    feat: add bottom border to the tab bar

--- app/src/main/java/com/zxc/mydays/MainActivity.kt
+++ app/src/main/java/com/zxc/mydays/MainActivity.kt
@@ -9,10 +9,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
@@ -27,9 +30,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import com.zxc.mydays.ui.theme.MyDaysTheme
 
@@ -40,9 +46,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            val tabs = listOf("小记", "待办", "日常")
-            val pagerState = rememberPagerState(pageCount = { 3 })
             MyDaysTheme {
+                val tabs = listOf("小记", "待办", "日常")
+                val pagerState = rememberPagerState(pageCount = { 3 })
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                     floatingActionButton = {
@@ -82,12 +88,24 @@ class MainActivity : ComponentActivity() {
                                 horizontalArrangement = Arrangement.spacedBy(24.dp)
                             ) {
                                 for ((index, tab) in tabs.withIndex()) {
-                                    Column {
+                                    Column(
+                                        modifier = Modifier
+                                            .wrapContentWidth(),
+                                        horizontalAlignment = Alignment.CenterHorizontally,
+                                    ) {
                                         Text(
                                             text = tab,
                                             color = if (index == pagerState.currentPage) MaterialTheme.colorScheme.primary else Color.Gray,
                                             fontWeight = if (index == pagerState.currentPage) FontWeight.Bold else FontWeight.Normal,
                                         )
+                                        Spacer(modifier=Modifier.height(4.dp))
+                                        HorizontalDivider(
+                                            modifier = Modifier
+                                                .width(48.dp),
+                                            thickness = 2.dp,
+                                            color = if (index == pagerState.currentPage) MaterialTheme.colorScheme.primary else Color.Transparent
+                                        )
+
                                     }
                                 }
                             }
@@ -108,14 +126,17 @@ class MainActivity : ComponentActivity() {
                             LazyColumn(
                                 modifier = Modifier
                                     .fillMaxSize()
-                                    .background(Color.Green)
+                                    .background(Color.Green),
+                                horizontalAlignment = Alignment.CenterHorizontally,
                             ) {
+                                item{
+                                    Text(pagerState.currentPageOffsetFraction.toString())
+                                }
                                 items(100) { index ->
-                                    Greeting(name = "Item $index")
+                                    Text(text = "Item $index")
                                 }
                             }
                         }
-                        Text(text = "test");
                     }
                 }
             }
