f6cf52af13ae01ebaf63e34ddf5c11b6aedfd032
zxc <acan.coder@gmail.com>
2024-11-12 22:42:05 +0800

    chore: optimize the code

--- app/src/main/java/com/zxc/mydays/MainActivity.kt
+++ app/src/main/java/com/zxc/mydays/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,7 +18,6 @@ import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -30,17 +28,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import com.zxc.mydays.ui.theme.MyDaysTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
+    @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -87,26 +82,25 @@ class MainActivity : ComponentActivity() {
                             Row(
                                 horizontalArrangement = Arrangement.spacedBy(24.dp)
                             ) {
-//                                Text(pagerState.currentPageOffsetFraction.toString())
-                                // TODO +0.25以上 右边的tab文字颜色变化 -0.25以下，左边的tab文字颜色变化
                                 for ((index, tab) in tabs.withIndex()) {
                                     Column(
-                                        modifier = Modifier
-                                            .wrapContentWidth(),
+                                        modifier = Modifier.wrapContentWidth(),
                                         horizontalAlignment = Alignment.CenterHorizontally,
                                     ) {
+                                        val activeTab = index == pagerState.currentPage
+                                        val nextActiveTab =
+                                            pagerState.currentPageOffsetFraction >= 0.25
+                                                    && pagerState.currentPageOffsetFraction < 0.5
+                                                    && index == pagerState.currentPage + 1
+                                                    ||
+                                                    pagerState.currentPageOffsetFraction <= -0.25
+                                                    && pagerState.currentPageOffsetFraction > -0.5
+                                                    && index == pagerState.currentPage - 1
                                         Text(
                                             text = tab,
-                                            color = if (index == pagerState.currentPage) {
+                                            color = if (activeTab) {
                                                 MaterialTheme.colorScheme.primary
-                                            } else if (pagerState.currentPageOffsetFraction >= 0.25
-                                                && pagerState.currentPageOffsetFraction < 0.5
-                                                && index == pagerState.currentPage + 1
-                                                ||
-                                                pagerState.currentPageOffsetFraction <= -0.25
-                                                && pagerState.currentPageOffsetFraction > -0.5
-                                                && index == pagerState.currentPage - 1
-                                            ) {
+                                            } else if (nextActiveTab) {
                                                 Color(0xFF776c96)
                                             } else {
                                                 Color.Gray
@@ -115,19 +109,11 @@ class MainActivity : ComponentActivity() {
                                         )
                                         Spacer(modifier = Modifier.height(4.dp))
                                         HorizontalDivider(
-                                            modifier = Modifier
-                                                .width(48.dp),
-                                            thickness = if (index == pagerState.currentPage) 2.dp else 1.dp,
-                                            color = if (index == pagerState.currentPage) {
+                                            modifier = Modifier.width(48.dp),
+                                            thickness = if (activeTab) 2.dp else 1.dp,
+                                            color = if (activeTab) {
                                                 MaterialTheme.colorScheme.primary
-                                            } else if (pagerState.currentPageOffsetFraction >= 0.25
-                                                && pagerState.currentPageOffsetFraction < 0.5
-                                                && index == pagerState.currentPage + 1
-                                                ||
-                                                pagerState.currentPageOffsetFraction <= -0.25
-                                                && pagerState.currentPageOffsetFraction > -0.5
-                                                && index == pagerState.currentPage - 1
-                                            ) {
+                                            } else if (nextActiveTab) {
                                                 Color(0xFFbbb6cb)
                                             } else {
                                                 Color.Transparent
@@ -146,10 +132,8 @@ class MainActivity : ComponentActivity() {
                         }
                         HorizontalPager(
                             state = pagerState,
-                            modifier = Modifier
-                                .fillMaxSize()
+                            modifier = Modifier.fillMaxSize(),
                         ) { page ->
-                            // Our page content
                             LazyColumn(
                                 modifier = Modifier
                                     .fillMaxSize(),
