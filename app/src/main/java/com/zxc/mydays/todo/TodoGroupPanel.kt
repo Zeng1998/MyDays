41e42e349d172ba9cee2c9bacc288490c377e84a
zxc <acan.coder@gmail.com>
2024-11-23 18:41:27 +0800

    chore: update some ui

--- app/src/main/java/com/zxc/mydays/todo/TodoGroupPanel.kt
+++ app/src/main/java/com/zxc/mydays/todo/TodoGroupPanel.kt
@@ -6,9 +6,7 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -60,7 +58,7 @@ fun TodoGroupPanel(
                 ) {
                     Text(
                         "$title $count",
-                        fontSize = 18.sp,
+                        fontSize = 16.sp,
                         fontWeight = FontWeight.Bold,
                         color = Color.DarkGray
                     )
--- app/src/main/java/com/zxc/mydays/todo/TodoScreen.kt
+++ app/src/main/java/com/zxc/mydays/todo/TodoScreen.kt
@@ -9,30 +9,51 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.zxc.mydays.common.TopTagPanel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TodoScreen() {
     val sheetState = rememberModalBottomSheetState()
     val scope = rememberCoroutineScope()
+    val tags = listOf(
+        "日记",
+        "菜谱",
+        "tag1",
+        "tag2",
+        "tag3",
+        "tag4",
+        "tag5",
+        "tag6",
+        "tag7",
+        "tag8",
+        "tag9",
+        "tag10"
+    )
+    var selectedTagIndex by remember { mutableIntStateOf(0) }
     var showBottomSheet by remember { mutableStateOf(false) }
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(vertical = 8.dp)
     ) {
+        TopTagPanel(
+            tags = tags,
+            selectedTagIndex = selectedTagIndex,
+            selectedTagIndexChange = { selectedTagIndex = it }
+        )
         TodoGroupPanel("进行中", 5) {
             TodoTaskItem(
                 text = "任务5",
                 isChecked = false,
                 onCheck = {},
-                // TODO 有子任务的才需要bottom sheet？那如果要修改呢？
                 onClick = { showBottomSheet = true }
             )
             TodoTaskItem(
