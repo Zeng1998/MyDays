1075364ea88e7ab16353035d4bcd0e5bb8e079d7
zxc <acan.coder@gmail.com>
2024-12-10 22:07:22 +0800

    feat: update the TodoDetailSheet

--- app/src/main/java/com/zxc/mydays/todo/TodoDetailSheet.kt
+++ app/src/main/java/com/zxc/mydays/todo/TodoDetailSheet.kt
@@ -1,17 +1,16 @@
 package com.zxc.mydays.todo
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -30,14 +29,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.zxc.mydays.R
@@ -53,6 +50,11 @@ fun TodoDetailSheet(
     val random = (1..3).random()
     val titleFocusRequester = remember { FocusRequester() }
     val titleBoxHeight = with(LocalDensity.current) { 42.sp.toDp() }
+    val subItems = if (random % 2 == 0) {
+        listOf("任务5", "任务6", "任务7")
+    } else {
+        emptyList()
+    }
     ModalBottomSheet(
         onDismissRequest = {
             onDismissRequest()
@@ -111,61 +113,57 @@ fun TodoDetailSheet(
                     horizontalArrangement = Arrangement.spacedBy(12.dp),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    when(random){
-                        1->Icon(
+                    when (random) {
+                        1 -> Icon(
                             painter = painterResource(id = R.drawable.meh),
                             contentDescription = null,
                             modifier = Modifier.size(18.dp),
                             tint = Color(0xFF228be6),
                         )
-                        2->Icon(
+
+                        2 -> Icon(
                             painter = painterResource(id = R.drawable.smile),
                             contentDescription = null,
                             modifier = Modifier.size(18.dp),
                             tint = Color(0xFF51cf66),
                         )
-                        3->Icon(
+
+                        3 -> Icon(
                             painter = painterResource(id = R.drawable.frown),
                             contentDescription = null,
                             modifier = Modifier.size(18.dp),
                             tint = Color(0xFFf03e3e),
                         )
                     }
-                    when(random){
-                        1->Text(text = "进行中 (1/5)", color = Color.Gray)
-                        2->Text(text = "已完成 (5/5)", color = Color.Gray)
-                        3->Text(text = "已废弃 (0/4)", color = Color.Gray)
+                    when (random) {
+                        1 -> Text(text = "进行中 (1/5)", color = Color.Gray)
+                        2 -> Text(text = "已完成 (5/5)", color = Color.Gray)
+                        3 -> Text(text = "已废弃 (0/4)", color = Color.Gray)
                     }
                 }
                 Icon(
                     painter = painterResource(id = R.drawable.plus),
                     contentDescription = null,
-                    modifier = Modifier.clickable {  }
+                    modifier = Modifier.clickable { }
                 )
             }
             Column(
                 modifier = Modifier.padding(start = 4.dp)
             ) {
-                TodoEditableItem(
-                    text = "任务5",
-                    isChecked = false,
-                    onCheck = {},
-                    onContentChange = {}
-                )
-                TodoEditableItem(
-                    text = "任务6",
-                    isChecked = false,
-                    onCheck = {},
-                    onContentChange = {}
-                )
-                TodoEditableItem(
-                    text = "任务7",
-                    isChecked = false,
-                    onCheck = {},
-                    onContentChange = {}
-                )
+                subItems.forEach {
+                    TodoEditableItem(
+                        text = it,
+                        isChecked = false,
+                        onCheck = {},
+                        onContentChange = {}
+                    )
+                }
             }
-            HorizontalDivider(modifier = Modifier.padding(vertical = 8.dp))
+            if (subItems.isNotEmpty()) {
+                HorizontalDivider(modifier = Modifier.padding(vertical = 8.dp))
+            }else{
+                Spacer(modifier = Modifier.height(8.dp))
+            }
             Button(
                 modifier = Modifier.fillMaxWidth(),
                 colors = ButtonDefaults.buttonColors(
